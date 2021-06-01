de86d2e99ddb42f8bc18eebf0be8ebea713f1ccf
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-06-01 16:52:08 +0100

    Fulfilled User Story 2: I want to be able to deposit it into my bank accountDeposit method added

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Account do
     expect(subject.balance).to eq(0)
   end
 
+  describe '#deposit' do
+    it 'increases the current balance by the given transaction amount' do
+      expect { subject.deposit(250) }.to change { subject.balance }.by 250
+    end
+  end
+
 end
